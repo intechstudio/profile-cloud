251b941405c251e0698cf07da4903c7c89429c8c
kkerti <kkerti@riseup.net>
2023-03-21 17:13:37 +0100

    KKERTI added upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,51 @@
 <script lang="ts">
 	import { db } from '$lib/firebase';
-	import { collection, doc, getDoc, getDocs, query } from 'firebase/firestore';
-	import { onMount } from 'svelte';
+	import { collection, doc, Firestore, getDoc, getDocs, query, setDoc } from 'firebase/firestore';
+
+	interface IProfile {
+		owner: string;
+		name: string;
+		slug: string;
+		description: string;
+		isGridProfile: boolean;
+		version: {
+			major: number;
+			minor: number;
+			patch: number;
+		};
+		productType: string;
+		pageJson: string;
+	}
+
+	let profileDoc: IProfile = {
+		owner: '',
+		name: '',
+		slug: '',
+		description: '',
+		version: {
+			major: 1,
+			minor: 2,
+			patch: 3
+		},
+		isGridProfile: true,
+		productType: '',
+		pageJson: ''
+	};
 
 	async function getProfiles() {
 		const q = query(collection(db, 'profiles'));
 		return await getDocs(q).then((res) => res.docs);
+	}
+
+	let profileToUpload: IProfile;
+	let isFirestoreUploading = false;
+	async function uploadProfile() {
+		profileToUpload = profileDoc;
+		profileToUpload.slug = profileDoc.name.toLowerCase().replace(/ /g, '-');
+		profileToUpload.pageJson = JSON.parse(profileDoc.pageJson);
+		isFirestoreUploading = true;
+		await setDoc(doc(db, 'profiles', profileToUpload.slug), profileToUpload);
+		isFirestoreUploading = false;
 	}
 </script>
 
@@ -14,7 +54,61 @@
 {#await getProfiles() then profiles}
 	{#each profiles as profile}
 		{@const data = profile.data()}
-		<a href="/{data.owner}/{data.name}">{data.owner}</a>
+		<a href="/{data.owner}/{data.slug}">{data.owner}</a>
 		<div>{data.name}</div>
 	{/each}
 {/await}
+
+<h1>Upload profile</h1>
+
+<div>
+	<label for="profile-owner">Owner</label>
+	<input type="text" id="profile-owner" bind:value={profileDoc.owner} placeholder="owner name" />
+</div>
+
+<div>
+	<label for="profile-name">Name</label>
+	<input
+		type="text"
+		id="profile-name"
+		bind:value={profileDoc.name}
+		placeholder="Name of the profile"
+	/>
+</div>
+<div>
+	<label for="profile-description">Description</label>
+	<textarea
+		name=""
+		id="profile-description"
+		bind:value={profileDoc.description}
+		cols="30"
+		rows="5"
+	/>
+</div>
+<div>
+	<label for="profile-product-type">Product Type</label>
+	<select bind:value={profileDoc.productType} name="" id="profile-product-type">
+		{#each ['BU16', 'PBF4', 'PO16', 'EF44', 'EN16'] as type}
+			<option value={type}>{type}</option>
+		{/each}
+	</select>
+</div>
+
+<div>
+	<label for="profile-page-json">Page JSON</label>
+	<textarea name="" id="profile-page-json" bind:value={profileDoc.pageJson} cols="30" rows="5" />
+</div>
+
+<div>
+	<button
+		on:click={() => {
+			uploadProfile();
+		}}>Upload Profile</button
+	>
+	{#if isFirestoreUploading}
+		<div>Uploading...</div>
+	{/if}
+	{#if profileToUpload}
+		<pre>{JSON.stringify(profileToUpload)}</pre>
+	{/if}
+</div>
--- src/routes/[owner]/[profile]/+page.svelte
+++ src/routes/[owner]/[profile]/+page.svelte
@@ -1,7 +1,26 @@
-<script>
+<script lang="ts">
+	import { db } from '$lib/firebase';
+	import { doc, getDoc } from 'firebase/firestore';
+
 	export let data;
 
 	console.log('fe', data);
+
+	async function loadDocument() {
+		const docRef = doc(db, 'profiles', data.params.profile);
+		const docSnap = await getDoc(docRef);
+		downloadObjectAsJson(docSnap.data(), data.params.profile);
+	}
+
+	function downloadObjectAsJson(exportObj: any, exportName: string) {
+		var dataStr = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(exportObj));
+		var downloadAnchorNode = document.createElement('a');
+		downloadAnchorNode.setAttribute('href', dataStr);
+		downloadAnchorNode.setAttribute('download', exportName + '.json');
+		document.body.appendChild(downloadAnchorNode); // required for firefox
+		downloadAnchorNode.click();
+		downloadAnchorNode.remove();
+	}
 </script>
 
 <svelte:head>
@@ -10,3 +29,9 @@
 
 <h1>{data.params.owner}</h1>
 {data.params.profile}
+
+<button
+	on:click={() => {
+		loadDocument();
+	}}>download</button
+>
