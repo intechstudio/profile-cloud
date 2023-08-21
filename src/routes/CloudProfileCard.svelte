2ee6a0a211d0abfc76caf6a85f535da14e2cb720
Pásztor Dániel <danim1130@users.noreply.github.com>
2023-08-21 14:57:56 +0200

    Merge conditional css

--- src/routes/CloudProfileCard.svelte
+++ src/routes/CloudProfileCard.svelte
@@ -230,13 +230,9 @@
 		class=" w-full flex py-1 px-3 justify-between items-center md:border-t-2 border-neutral-200 dark:border-neutral-700"
 	>
 		<div
-			class="dark:text-white text-black text-opacity-80 {data.type === data.selectedModuleType
-				? 'dark:text-opacity-100'
-				: 'dark:text-opacity-70'}
-				dark:border py-0.5 px-2 {data.type ===
-					data.selectedModuleType
-						? 'dark:border-white dark:border-opacity-10 dark:bg-white dark:bg-opacity-10'
-						: 'dark:border-transparent'}"
+			class="dark:text-white text-black text-opacity-80 py-0.5 px-2 dark:border {data.type === data.selectedModuleType
+				? 'dark:text-opacity-100 dark:border-white dark:border-opacity-10 dark:bg-white dark:bg-opacity-10'
+				: 'dark:text-opacity-70 dark:border-transparent'}
 		>
 			{data.type}
 		</div>
