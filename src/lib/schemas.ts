a8c01d423d576a20bbf4497b642188ef5296275a
suku <sukuwc@riseup.net>
2023-09-06 20:55:06 +0200

    SUKU formatting fixed

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,27 +1,27 @@
 import { z } from "zod";
 
 export const ProfileSchema = z.object({
-    id: z.string().optional(),
-    owner: z.string().nullable(),
-    access: z.array(z.string()),
-    createdAt: z.date().nullable().optional(),
-    public: z.boolean().default(false),
-    name: z.string(),
-    description: z.string(),
-    type: z.string(),
-    version: z
-        .object({
-            major: z.string(),
-            minor: z.string(),
-            patch: z.string()
-        })
-        .optional(),
-    isGridProfile: z.boolean(),
-    configs: z.any()
+  id: z.string().optional(),
+  owner: z.string().nullable(),
+  access: z.array(z.string()),
+  createdAt: z.date().nullable().optional(),
+  public: z.boolean().default(false),
+  name: z.string(),
+  description: z.string(),
+  type: z.string(),
+  version: z
+    .object({
+      major: z.string(),
+      minor: z.string(),
+      patch: z.string(),
+    })
+    .optional(),
+  isGridProfile: z.boolean(),
+  configs: z.any(),
 });
 
 export const ProfileLinkSchema = ProfileSchema.extend({
-    linked: z.boolean().default(true)
+  linked: z.boolean().default(true),
 });
 
 export type Profile = z.infer<typeof ProfileSchema>;
