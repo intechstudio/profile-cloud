7870ff3c1056484acc88e0354c5fa2272380fdf4
elsoazemelet <elsoazemelet@gmail.com>
2023-09-07 08:58:13 +0200

    Revert "Format"
    This reverts commit 5f517f60179db0bc8c3c4ba52257f2a8b8f557f8.

--- src/lib/components/GoogleAuth.svelte
+++ src/lib/components/GoogleAuth.svelte
@@ -1,29 +1,29 @@
 <script lang="ts">
-    import { PUBLIC_GOOGLE_CLIENT_ID } from "$env/static/public";
-    import { createEventDispatcher } from "svelte";
+	import { PUBLIC_GOOGLE_CLIENT_ID } from '$env/static/public';
+	import { createEventDispatcher } from 'svelte';
 
-    const dispatch = createEventDispatcher();
+	const dispatch = createEventDispatcher();
 
-    function callbackFunction(response: any) {
-        if (response.credential) {
-            dispatch("google-signin", response.credential);
-        }
-    }
+	function callbackFunction(response: any) {
+		if (response.credential) {
+			dispatch('google-signin', response.credential);
+		}
+	}
 
-    function initGapi() {
-        google.accounts.id.initialize({
-            client_id: PUBLIC_GOOGLE_CLIENT_ID,
-            callback: callbackFunction
-        });
-        google.accounts.id.renderButton(
-            document.getElementById("buttonDiv"),
-            { theme: "outline", size: "large", width: "240px", text: "continue_with" } // customization attributes
-        );
-    }
+	function initGapi() {
+		google.accounts.id.initialize({
+			client_id: PUBLIC_GOOGLE_CLIENT_ID,
+			callback: callbackFunction
+		});
+		google.accounts.id.renderButton(
+			document.getElementById('buttonDiv'),
+			{ theme: 'outline', size: 'large', width: '240px', text: 'continue_with' } // customization attributes
+		);
+	}
 </script>
 
 <svelte:head>
-    <script src="https://accounts.google.com/gsi/client" async defer on:load={initGapi}></script>
+	<script src="https://accounts.google.com/gsi/client" async defer on:load={initGapi}></script>
 </svelte:head>
 
 <div id="buttonDiv" />
