3e82fdf05c7ad9efb9606570f8881df7ea4082c0
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2023-10-15 12:48:01 +0200

    KKERTI added custom read functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,32 +1,10 @@
-import * as functions from "firebase-functions";
-import * as firestore from "@google-cloud/firestore";
-const client = new firestore.v1.FirestoreAdminClient();
+import {scheduledFirestoreExport as SFE} from "./firestoreBackup";
+import {
+    readAllProfiles as RAP,
+    readSingleProfile as RSP,
+} from "./publicCloudQueries";
 
-// Replace BUCKET_NAME
-const bucket = "gs://profile-cloud-firestore-backup";
-
-export const scheduledFirestoreExport = functions
-    .region("europe-west3")
-    .pubsub.schedule("every 24 hours")
-    .timeZone("Europe/Zurich")
-    .onRun((context) => {
-        const projectId = process.env.GCP_PROJECT || "profile-cloud";
-        const databaseName = client.databasePath(projectId, "(default)");
-        return client
-            .exportDocuments({
-                name: databaseName,
-                outputUriPrefix: bucket,
-                // Leave collectionIds empty to export all collections
-                // or set to a list of collection IDs to export,
-                // collectionIds: ['users', 'posts']
-                collectionIds: []
-            })
-            .then((responses) => {
-                const response = responses[0];
-                console.log(`Operation Name: ${response["name"]}`);
-            })
-            .catch((err) => {
-                console.error(err);
-                throw new Error("Export operation failed");
-            });
-    });
+// this is just sad. should learn how to import and export properly
+export const scheduledFirestoreExport = SFE;
+export const readAllProfiles = RAP;
+export const readSingleProfile = RSP;
---
+++ functions/src/publicCloudQueries.ts
@@ -0,0 +1,34 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp();
+const db = admin.firestore();
+
+export const readAllProfiles = functions.https.onRequest((req, res) => {
+    // todo in future: limit, orderby, where
+    db.collection("profiles")
+        .get()
+        .then((snapshot) => {
+            const data = snapshot.docs.map((doc) => doc.data());
+            res.json(data);
+        })
+        .catch((err) => {
+            res.json({message: "error", error: err});
+        });
+});
+
+export const readSingleProfile = functions.https.onRequest((req, res) => {
+    db.collection("profiles")
+        .where("id", "==", req.body.id)
+        .get()
+        .then((snapshot) => {
+            const data = snapshot.docs.map((doc) => doc.data());
+            if (data.length === 0) {
+                res.json({message: "no profile found"});
+            }
+            // return single item
+            res.json(data[0]);
+        })
+        .catch((err) => {
+            res.json({message: "error", error: err});
+        });
+});
