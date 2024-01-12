d2be0a2768825e239c736d29424ec41f6dfa5224
elsoazemelet <elsoazemelet@gmail.com>
2024-01-12 17:08:39 +0100

    Daily push

---
+++ src/routes/BrowserLayout.svelte
@@ -0,0 +1,337 @@
+<script lang="ts">
+    import { onDestroy, onMount } from "svelte";
+    import { type Config } from "$lib/schemas";
+    import { parentIframeCommunication } from "$lib/utils";
+    import { type ConfigManager, createConfigManager } from "$lib/configmanager/ConfigManager";
+    import ConfigCard from "./ConfigCard.svelte";
+
+    enum SortFieldType {
+        name = "name",
+        date = "date",
+        module = "module"
+    }
+
+    let searchSuggestions: string[] = [];
+
+    let selectedConfigIndex: number | undefined = undefined;
+
+    let configManager: ConfigManager | undefined = undefined;
+    let configs: Config[] = [];
+    let filteredConfigs: Config[] = [];
+
+    let selectedComponentTypes: string[] = [];
+
+    let configTypeSelector = "profile";
+
+    let isSearchSortingShows = false;
+    let searchbarValue = "";
+
+    let sortAsc = false;
+    let sortField: SortFieldType = SortFieldType.date;
+
+    let compareFieldsMap = new Map([
+        [
+            SortFieldType.name,
+            (a: any, b: any) => {
+                return a.name
+                    .toLowerCase()
+                    .localeCompare(b.name.toLowerCase(), undefined, { numeric: true });
+            }
+        ],
+        [
+            SortFieldType.module,
+            (a: any, b: any) => {
+                return a.type.localeCompare(b.type, undefined, {
+                    numeric: true
+                });
+            }
+        ],
+        [
+            SortFieldType.date,
+            (a: any, b: any) => {
+                return a.modifiedAt - b.modifiedAt;
+            }
+        ]
+    ]);
+
+    $: configTypeSelector, updateSearchSuggestions();
+
+    function updateSearchSuggestions() {
+        if (configTypeSelector === "profile") {
+            searchSuggestions = ["BU16", "EF44", "EN16", "PBF4", "PO16"];
+        } else if (configTypeSelector === "preset") {
+            searchSuggestions = ["button", "encoder", "potentiometer", "fader"];
+        }
+    }
+
+    $: configs, searchbarValue, configTypeSelector, updateSearchFilter();
+    function updateSearchFilter() {
+        const input = searchbarValue;
+        const arrayOfSearchTerms = input.trim().toLowerCase().split(" ");
+
+        filteredConfigs = configs.filter((config) => {
+            if (config.configType !== configTypeSelector) {
+                return false;
+            }
+            const currentProfileSearchable =
+                config.name.toLowerCase() + " " + config.type.toLowerCase();
+
+            for (const searchTerm of arrayOfSearchTerms) {
+                if (currentProfileSearchable.indexOf(searchTerm) === -1) {
+                    return false;
+                }
+            }
+            return true;
+        });
+        //Only sort when ordering explicitly changes (like file explorer)
+    }
+
+    $: sortField, sortAsc, sortProfileCloud();
+    function sortProfileCloud() {
+        if (sortField) {
+            configs.sort(compareFieldsMap.get(sortField));
+        }
+        if (sortAsc) {
+            configs.reverse();
+        }
+        updateSearchFilter();
+    }
+
+    $: configTypeSelector, resetPageState();
+
+    function resetPageState() {
+        selectedConfigIndex = undefined;
+        provideSelectedConfigForEditor({});
+    }
+
+    async function provideSelectedConfigForEditor(config?: Config | {}) {
+        await parentIframeCommunication({
+            windowPostMessageName: "provideSelectedConfigForEditor",
+            dataForParent: { config: config ?? {} }
+        });
+    }
+
+    function filterShowHide() {
+        //DONE
+        isSearchSortingShows = !isSearchSortingShows;
+    }
+
+    function useSearchSuggestion(suggestionText: string) {
+        //DONE
+        searchbarValue = suggestionText;
+    }
+
+    onMount(async () => {
+        configManager = createConfigManager({
+            next: (newConfigs) => {
+                newConfigs.sort((a, b) => {
+                    let ai = configs.findIndex((e) => e.id === a.id);
+                    let bi = configs.findIndex((e) => e.id === b.id);
+                    return ai - bi;
+                });
+                configs = newConfigs;
+            }
+        });
+    });
+
+    onDestroy(() => {
+        configManager?.cancel();
+        configManager = undefined;
+    });
+</script>
+
+<div class="flex-col py-4 h-full">
+    <div class="flex justify-end">
+        <button
+            on:click={() => {
+                filterShowHide();
+            }}
+            class="text-left font-xs"
+        >
+            {#if isSearchSortingShows}
+                Hide Filters
+            {:else}
+                Show Filters
+            {/if}
+        </button>
+    </div>
+    {#if isSearchSortingShows == true}
+        <!--
+        <div
+        in:fadeAnimation|local={{ fn: fade, y: 50, duration: 150 }}
+        out:fadeAnimation|local={{ fn: fade, y: 50, duration: 150 }}
+        >
+    -->
+        <div>
+            <div class="flex flex-col gap-1 px-3 pt-3">
+                <div class="relative">
+                    <svg
+                        class="absolute left-3 bottom-[28%]"
+                        width="14"
+                        height="14"
+                        viewBox="0 0 18 18"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M13.2095 11.6374C14.2989 10.1509 14.7868 8.30791 14.5756
+                6.47715C14.3645 4.64639 13.4699 2.96286 12.0708 1.76338C10.6717
+                0.563893 8.87126 -0.0630888 7.02973 0.0078685C5.1882 0.0788258
+                3.44137 0.84249 2.13872 2.14608C0.83606 3.44967 0.0736462 5.19704
+                0.00400665 7.03862C-0.0656329 8.8802 0.562637 10.6802 1.76312
+                12.0784C2.96361 13.4767 4.64778 14.3701 6.47869 14.5799C8.3096
+                14.7897 10.1522 14.3005 11.6379 13.2101H11.6368C11.6705 13.2551
+                11.7065 13.2979 11.747 13.3395L16.0783 17.6707C16.2892 17.8818
+                16.5754 18.0005 16.8738 18.0006C17.1723 18.0007 17.4585 17.8822
+                17.6696 17.6713C17.8807 17.4603 17.9994 17.1742 17.9995
+                16.8758C17.9996 16.5773 17.8811 16.2911 17.6702 16.08L13.3389
+                11.7487C13.2987 11.708 13.2554 11.6704 13.2095
+                11.6362V11.6374ZM13.4998 7.31286C13.4998 8.12541 13.3397 8.93001
+                13.0288 9.68071C12.7178 10.4314 12.2621 11.1135 11.6875
+                11.6881C11.113 12.2626 10.4308 12.7184 9.68014 13.0294C8.92944
+                13.3403 8.12484 13.5004 7.31229 13.5004C6.49974 13.5004 5.69514
+                13.3403 4.94444 13.0294C4.19373 12.7184 3.51163 12.2626 2.93707
+                11.6881C2.3625 11.1135 1.90674 10.4314 1.59578 9.68071C1.28483
+                8.93001 1.12479 8.12541 1.12479 7.31286C1.12479 5.67183 1.77669
+                4.09802 2.93707 2.93763C4.09745 1.77725 5.67126 1.12536 7.31229
+                1.12536C8.95332 1.12536 10.5271 1.77725 11.6875 2.93763C12.8479
+                4.09802 13.4998 5.67183 13.4998 7.31286V7.31286Z"
+                            fill="#CDCDCD"
+                        />
+                    </svg>
+
+                    {#if searchbarValue != ""}
+                        <button
+                            class="absolute right-2 bottom-[25%]"
+                            on:click={() => (searchbarValue = "")}
+                        >
+                            <svg
+                                width="28"
+                                height="28"
+                                viewBox="0 0 39 39"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M24.25 32.9102L14.75 23.4102M24.25 23.4102L14.75 32.9102"
+                                    stroke="#FFF"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                />
+                            </svg>
+                        </button>
+                    {/if}
+
+                    <input
+                        type="text"
+                        bind:value={searchbarValue}
+                        on:keyup={() => updateSearchFilter()}
+                        on:input={() => updateSearchFilter()}
+                        on:change={() => updateSearchFilter()}
+                        class="w-full py-2 px-12 bg-white dark:bg-primary-700
+            dark:placeholder-gray-400 text-md focus:outline-none"
+                        placeholder="Find Profile..."
+                    />
+                </div>
+
+                <div class="flex flex-row gap-1 py-1 flex-wrap">
+                    {#each searchSuggestions as suggestion}
+                        <button
+                            on:click={() => useSearchSuggestion(suggestion)}
+                            class="border hover:border-primary-500 text-xs dark:text-primary-100 rounded-md
+            py-0.5 px-1 h-min {searchbarValue.toLowerCase() == suggestion.toLowerCase()
+                                ? 'border-primary-100'
+                                : 'border-primary-700'}"
+                        >
+                            {suggestion}
+                        </button>
+                    {/each}
+                </div>
+            </div>
+
+            <div class="flex gap-2 items-center justify-between flex-wrap p-3">
+                <label for="sorting select" class="uppercase text-gray-500 py-1 text-xs">
+                    sort by
+                </label>
+
+                <select
+                    class="bg-white dark:bg-secondary border-none flex-grow p-1 focus:outline-none"
+                    id="sortingSelectBox"
+                    on:change={(e) => {
+                        sortField = e.target.value;
+                    }}
+                    name="sorting select"
+                >
+                    <option class="text-white bg-secondary py-1 border-none" value="date">
+                        date
+                    </option>
+                    <option
+                        selected
+                        class="bg-white dark:bg-secondary py-1 border-none"
+                        value="name"
+                    >
+                        name
+                    </option>
+
+                    <option class="bg-white dark:bg-secondary py-1 border-none" value="module">
+                        module
+                    </option>
+                </select>
+
+                <button
+                    on:click={() => {
+                        sortAsc = !sortAsc;
+                    }}
+                >
+                    {#if sortAsc == false}
+                        <svg
+                            width="20"
+                            height="20"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                class="stroke-black dark:stroke-white stroke-2"
+                                d="M11 11H15M11 15H18M11 19H21M9 7L6 4L3 7M6 6V20"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                        </svg>
+                    {:else}
+                        <svg
+                            width="20"
+                            height="20"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M11 5H21M11 9H18M11 13H15M3 17L6 20L9 17M6 18V4"
+                                class="stroke-black dark:stroke-white stroke-2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                        </svg>
+                    {/if}
+                </button>
+            </div>
+        </div>
+    {/if}
+    <div
+        class="overflow-y-auto w-full h-full p-2 lg:py-8 grid grid-cols-1 md:grid-cols-2 grid-flow-row lg:grid-cols-3 gap-4"
+    >
+        {#each filteredConfigs as config, index (config.id)}
+            <ConfigCard
+                on:click={() => {
+                    selectedConfigIndex = index;
+                }}
+                isSelected={index === selectedConfigIndex}
+                data={{
+                    ...config,
+                    selectedComponentTypes: selectedComponentTypes
+                }}
+            />
+        {/each}
+    </div>
+</div>
