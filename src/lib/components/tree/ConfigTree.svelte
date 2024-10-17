c3ffdc1562fa24134573b798520b9bc1887196d7
elsoazemelet <elsoazemelet@gmail.com>
2024-10-17 20:10:18 +0200

    Daily push

--- src/lib/components/tree/ConfigTree.svelte
+++ src/lib/components/tree/ConfigTree.svelte
@@ -3,18 +3,31 @@
     import { filterConfigs } from "./../../../routes/Filter";
     import { sort_key, sortConfigs } from "./../../../routes/Sorter";
     import { filter_value, FilterValue } from "./../../../routes/Filter";
-    import { selected_config, show_supported_only } from "./../../../routes/EditorLayout";
-    import TreeNode from "./TreeNode.svelte";
+    import {
+        selected_config,
+        show_supported_only,
+        config_manager
+    } from "./../../../routes/EditorLayout";
     import { type Config } from "$lib/schemas";
     import { TreeNodeData, createTree } from "./ConfigTree";
-    import { createEventDispatcher, tick } from "svelte";
+    import { tick } from "svelte";
+    import { createTreeView } from "@melt-ui/svelte";
+    import { setContext } from "svelte";
+    import Tree from "./Tree.svelte";
+
+    const ctx = createTreeView({
+        defaultExpanded: []
+    });
+    setContext("tree", ctx);
+
+    const {
+        elements: { tree },
+        states: { expanded }
+    } = ctx;
 
     export let configs: Config[];
 
-    type ConfigTreeData = TreeNodeData<Config>[];
-    let data: ConfigTreeData = [];
-
-    const dispatch = createEventDispatcher();
+    let root: TreeNodeData<Config>;
 
     async function scrollToSelectedConfig() {
         await tick();
@@ -27,89 +40,95 @@
         });
     }
 
-    function handleDataChange(data: ConfigTreeData) {
-        const openContainingCategories = (
-            id: string | undefined,
-            current: TreeNodeData<Config>
-        ): string | undefined => {
-            for (const node of current.nodes) {
-                const opened = openContainingCategories(id, node);
-                if (opened) {
-                    current.open.set(true);
-                }
-            }
-
-            const found = current.children.find((e) => e.id === id);
-            if (found) {
-                current.open.set(true);
-            }
-
-            return get(current.open);
-        };
-
-        const selected = get(selected_config)?.id;
-        if (typeof selected === "undefined") {
-            return undefined;
-        }
-
-        for (const root of data) {
-            openContainingCategories(selected, root);
-        }
-
-        scrollToSelectedConfig();
-    }
-
     function selectClosestMatch() {
-        for (const root of data) {
-            const temp = root.toArray();
-            if (temp.length > 0) {
-                selected_config.set({ id: temp[0].id, presetIndex: -1 });
-                return;
-            }
+        const temp = root.toArray();
+        if (temp.length > 0) {
+            selected_config.set({ id: temp[0].id, presetIndex: -1 });
+            return;
+        } else {
+            selected_config.set(undefined);
         }
-
-        selected_config.set(undefined);
     }
 
     let filterBuffer = new FilterValue();
 
     $: {
-        data = createTree(configs, $show_supported_only);
-        data.forEach((root) => {
-            root.filter($filter_value, filterConfigs);
-            root.sort($sort_key, sortConfigs);
-        });
+        root = createTree(configs, $show_supported_only);
+        root.filter($filter_value, filterConfigs);
+        root.sort($sort_key, sortConfigs);
 
         if (!filterBuffer.isEqual($filter_value)) {
             selectClosestMatch();
         }
 
         filterBuffer = $filter_value;
+
+        if ($selected_config) {
+            handleSelectedConfigChange($selected_config.id);
+        }
     }
 
-    $: handleDataChange(data);
+    $: {
+        if ($selected_config) {
+            handleSelectedConfigChange($selected_config.id);
+        }
+    }
 
-    function handleToggle(toggled: TreeNodeData<Config>, value: boolean) {
-        if (value) {
-            for (let node of data) {
-                if (node.id !== toggled.id) {
-                    node.open.set(false);
-                }
+    function handleSelectedConfigChange(id: string) {
+        expanded.set([]);
+        for (const child of root.children) {
+            toggleIncludingNodes(id, child);
+        }
+        scrollToSelectedConfig();
+    }
+
+    function nodeIncludesItem(id: string, node: TreeNodeData<Config>) {
+        if (typeof node.items.find((e) => e.id === id) !== "undefined") {
+            return true;
+        }
+
+        for (const child of node.children) {
+            if (nodeIncludesItem(id, child)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function toggleIncludingNodes(id: string, node: TreeNodeData<Config>) {
+        const contains = nodeIncludesItem(id, node);
+        if (contains) {
+            expanded.update((store) => {
+                store.push(node.id);
+                return store;
+            });
+        }
+
+        if (node.children.length > 0) {
+            for (const child of node.children) {
+                toggleIncludingNodes(id, child);
+            }
+        }
+    }
+
+    function handleDeleteVirtualDirectory(e: any) {
+        const { title } = e.detail;
+        const cm = get(config_manager);
+
+        for (const config of configs) {
+            if (config.virtualPath?.includes(title)) {
+                const newPath = config.virtualPath
+                    .split("/")
+                    .filter((e: string) => e !== title)
+                    .join("/");
+                config.virtualPath = newPath === "" ? undefined : newPath;
+                cm?.saveConfig(config, false);
             }
         }
     }
 </script>
 
-<div class="flex flex-col w-full h-full max-h-full">
-    {#each data as rootNode}
-        {#key rootNode.open}
-            <TreeNode
-                data={rootNode}
-                on:toggle={(e) => {
-                    const { value } = e.detail;
-                    handleToggle(rootNode, value);
-                }}
-            />
-        {/key}
-    {/each}
-</div>
+<ul class="flex flex-col w-full h-full max-h-full" {...$tree}>
+    <Tree treeItems={root.children} on:delete-virtual-directory={handleDeleteVirtualDirectory} />
+</ul>
