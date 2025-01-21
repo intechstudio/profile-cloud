eadb02e65352181737f3294b641e01ea694b0cf7
unknown <elsoazemelet@gmail.com>
2025-01-21 22:31:01 +0100

    Added searchable shorts

--- src/routes/Filter.ts
+++ src/routes/Filter.ts
@@ -1,5 +1,6 @@
 import { type Writable, writable } from "svelte/store";
 import { type Config } from "../lib/schemas";
+import { grid } from "@intechstudio/grid-protocol";
 
 export function stringToTerms(value: string, wholeMatch: boolean, caseMatch: boolean) {
     const terms: Term[] = [];
@@ -87,7 +88,34 @@ export function filterConfigs(configs: Config[], filter: FilterValue): Config[] 
     }
 
     return configs.filter((config) => {
-        // Collect all searchable fields
+        // Get scripts from Presets/Profiles
+        let scripts: string[] = [];
+        switch (config.configType) {
+            case "preset": {
+                scripts = config.configs.events.map((e: any) => e.config);
+                break;
+            }
+            case "profile": {
+                for (const element of config.configs) {
+                    scripts.push(...element.events.map((e: any) => e.config));
+                }
+                break;
+            }
+        }
+
+        // Regular expression to match --[[@<any string>]]
+        const regex = /--\[\[@(.*?)\]\]/g;
+
+        // Extract all unique shorts from scripts
+        const shorts = Array.from(
+            new Set(
+                scripts.flatMap((str) => {
+                    const matches = str.matchAll(regex); // Get all matches
+                    return Array.from(matches, (match) => match[1]); // Extract matched strings
+                })
+            )
+        );
+
         const searchables = [
             config.name,
             config.type,
@@ -96,8 +124,18 @@ export function filterConfigs(configs: Config[], filter: FilterValue): Config[] 
         ].filter((field): field is string => field !== undefined);
 
         // Check if any term matches any searchable field
-        return filter.every((term) =>
-            searchables.some((searchable) => matchesTerm(searchable, term))
-        );
+        return filter.every((term) => {
+            if (term.value.startsWith("$")) {
+                return shorts.some((short) =>
+                    matchesTerm(short, {
+                        value: term.value.slice(1, term.value.length),
+                        caseMatch: false,
+                        wholeMatch: false
+                    })
+                );
+            } else {
+                return searchables.some((searchable) => matchesTerm(searchable, term));
+            }
+        });
     });
 }
