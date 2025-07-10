f720f3d6a09fd3338f3be24c94b25be49aaa1c7c
unknown <elsoazemelet@gmail.com>
2025-07-10 16:42:58 +0200

    Fixed QA issue and UI-kit version bump

--- src/lib/components/tree/ConfigTree.svelte
+++ src/lib/components/tree/ConfigTree.svelte
@@ -61,7 +61,7 @@
       root: node,
       selected: $selected_config?.id,
       expanded: node.getIncludingNodes($selected_config?.id),
-      scrollToSelected: true,
+      scrollBehaviour: { scrollToIndex: true, easing: "instant" },
     };
   }
 
